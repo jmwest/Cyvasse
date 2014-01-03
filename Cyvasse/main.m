97831c5f4a37aed314f4ad891bfb79dc099bc756
JohnWest <johnwest@umich.edu>
2014-01-03 17:41:51 -0500

    Initial Commit

---
+++ Cyvasse/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Cyvasse
+//
+//  Created by John West on 1/3/14.
+//  Copyright (c) 2014 John West. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+	}
+}
